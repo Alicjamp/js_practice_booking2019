4f39ffabeea6ec2ac5a66665625fa90845852448
Alicjamp <alicja.parker@outlook.com>
2019-11-26 22:20:58 +0000

    Week 4 JS homework

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,45 +1,60 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.filter(function (n) {
+    return n < 1
+  })
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  return names.filter(function (name) {
+    return name.charAt(0) == char
+  })
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  return words.filter(function (verb) {
+    return verb.slice(0, 3).includes("to ")
+  })
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.filter(function (n) {
+    return (n % 1 === 0)
+  })
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  return users.map(function (user) {
+    return user.data.city.displayName
+  })
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  return nums.map(function (n) {
+    return parseFloat(Math.sqrt(n).toFixed(2))
+  })
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  return sentences.filter(function (sentence) {
+    return sentence.toLowerCase().includes(str)
+  })
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  return triangles.map(function (value) {
+    return (Math.max(...value))
+  })
 }
-
 module.exports = {
   findSmallNums,
   findNamesBeginningWith,
