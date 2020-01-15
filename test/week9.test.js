0104c6b715aa0d860c917642ce94d0cc4322c3e1
Alicjamp <alicja.parker@outlook.com>
2020-01-15 22:13:38 +0000

    sumMultiples working

--- test/week9.test.js
+++ test/week9.test.js
@@ -9,10 +9,10 @@ const {
 
 describe("return sum of any numbers which are a multiple of 3 or 5", () => {
     test("returns sum of 3,6 and 10", () => {
-        expect(sumMultiples(3,6,8,2,10,1)).toBe(19);
+        expect(sumMultiples([3,6,8,2,10,1])).toBe(19);
     });
 
     test("returns sum of 15,5,3 and 9", () => {
-        expect(sumMultiples(4,15,5,3,7,11,9)).toBe(32);
+        expect(sumMultiples([4,15,5,3,7,11,9])).toBe(32);
     });
 });
