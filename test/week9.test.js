607ecc13845f0294922979ce151c2e84bd0eb8f9
Alicjamp <alicja.parker@outlook.com>
2020-01-15 21:52:51 +0000

    test1

---
+++ test/week9.test.js
@@ -0,0 +1,18 @@
+const {
+    sumMultiples,
+    isValidDNA,
+    getComplementaryDNA,
+    isItPrime,
+    createMatrix,
+    areWeCovered
+} = require("../challenges/week9");
+
+describe("return sum of any numbers which are a multiple of 3 or 5", () => {
+    test("returns sum of 3,6 and 10", () => {
+        expect(sumMultiples(3,6,8,2,10,1)).toBe(19);
+    });
+
+    test("returns sum of 15,5,3 and 9", () => {
+        expect(sumMultiples(4,15,5,3,7,11,9)).toBe(32);
+    });
+});
